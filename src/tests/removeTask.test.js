07784df6d47b648a42d0dea8db17268116041155
Muhammad Ahmad Saeed <ehmaddd@gmail.com>
2023-04-06 15:02:08 +0500

    Update RemoveTask file

--- src/tests/removeTask.test.js
+++ src/tests/removeTask.test.js
@@ -1,14 +1,14 @@
-// jest.mock('../tasks');
+jest.mock('../tasks');
 
-// const { removeTask } = require('../tasks');
+const { removeTask } = require('../tasks');
 
-// describe('Remove Tasks', () => {
-//   test('Should remove a task to list', () => {
-//     expect(removeTask(
-//       {
-//         description: 'study',
-//         completed: false,
-//       },
-//     )).toStrictEqual([]);
-//   });
-// });
+describe('Remove Tasks', () => {
+  test('Should remove a task to list', () => {
+    expect(removeTask(
+      {
+        description: 'study',
+        completed: false,
+      },
+    )).toStrictEqual([]);
+  });
+});
